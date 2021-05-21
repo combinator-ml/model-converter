913dd0d9048001b35da895501ffa7a24fd5cd7d6
Phil Winder <phil@WinderResearch.com>
2021-05-21 15:05:58 +0100

    feat: initial commit

--- locals.tf
+++ locals.tf
@@ -1,3 +1,10 @@
 locals {
-  # Attempt to place all strings or generated fields here, rather than in the files
+  chart_repository = "https://feast-helm-charts.storage.googleapis.com" # TODO
+  chart_name       = "feast"                                            # TODO
+  chart_version    = "0.100.4"                                          # TODO
+  helm_values = {                                                       # TODO
+    redis = {
+      enabled = true
+    }
+  }
 }
