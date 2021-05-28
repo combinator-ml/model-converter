892bc4ca72dc488b718ba1c7ac73e4ef6e17de98
Carlos Millán Soler <carmilso@inf.upv.es>
2021-05-28 10:50:37 +0200

    Updated testfaster

--- examples/testfaster/main.tf
+++ examples/testfaster/main.tf
@@ -1,3 +1,57 @@
-module "modelconverter" {
-  source = "combinator-ml/modelconverter/k8s"
+variable "namespace" {
+  description = "The namespace to install into."
+  default     = "default"
+  type        = string
 }
+
+locals {
+  prefix           = "testfaster"
+  create_namespace = var.namespace == "default" ? 0 : 1 # Create if not default
+}
+
+resource "kubernetes_namespace" "namespace" {
+  count = local.create_namespace
+  metadata {
+    name = var.namespace
+  }
+}
+
+module "modelconverter" {
+  source = "../.."
+  namespace = var.namespace
+  depends_on = [
+    kubernetes_namespace.namespace
+  ]
+}
+
+module "jupyter" {
+  source    = "combinator-ml/jupyter/k8s"
+  version   = "0.0.3"
+  namespace = var.namespace
+  depends_on = [
+    kubernetes_namespace.namespace
+  ]
+}
+
+resource "kubernetes_service" "jupyter" {
+  metadata {
+    name      = "${local.prefix}-jupyter"
+    namespace = var.namespace
+  }
+  spec {
+    selector = {
+      app = "combinator-jupyter"
+    }
+    port {
+      name        = "http"
+      port        = 8888
+      target_port = 8888
+      node_port   = 30600
+    }
+    type = "NodePort"
+  }
+  depends_on = [
+    module.jupyter
+  ]
+}
+
