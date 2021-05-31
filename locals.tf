72eda17239c83b0ec15d2f4465cb9c4a7d51f4db
Carlos Millán Soler <carmilso@inf.upv.es>
2021-05-31 10:48:40 +0200

    Disable ingress

--- locals.tf
+++ locals.tf
@@ -3,6 +3,9 @@ locals {
   chart_name       = "containerizer"
   chart_version    = "0.1.0"
   helm_values = {
+    ingress = {
+      enabled = false
+    }
     env: [{
       name: "MINIO_ENDPOINT_URL",
       value: var.minio_address,
